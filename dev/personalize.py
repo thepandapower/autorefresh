738829ab80eb75af809e2f891863e125a432f567
Panda <victor.h0827@gmail.com>
2021-10-20 18:25:03 -0700

    clarifications and fix typo

--- dev/personalize.py
+++ dev/personalize.py
@@ -11,30 +11,32 @@ def saveLocation(f):
 #make file for locations
 locationFile = open('myLocations.txt', 'w')
 
-print('Please open Bluestacks, e7, and enter the secret shop')
+print('Please open Bluestacks, e7, and enter the secret shop\n')
 
 
 #find refresh button
-print('Please move your mouse to the center of the Refresh button and press `x`')
+print('Please move your mouse to the center of the Refresh button and press `x`\n')
 refresh_loc = saveLocation(locationFile)
 
 
 #find refresh confirm button
-print('Please click the Refresh button and move your mouse to the center of the Confirm button and press `x`, then press Cancel')
+print('Please click the Refresh button and move your mouse to the center of the Confirm button and press `x`, then press Cancel\n')
 refConfirm_loc = saveLocation(locationFile)
 
 
 #find item TL corner
+print('Refresh the shop until you find either friendship bm, bookmark, or mystics')
+print('Do NOT scroll anymore in the shop')
 print('WARNING: DO THIS NEXT INSTRUCTION CAREFULLY')
-print('Please move your mouse to the top left corner of any square item icon INCLUDING THE GREY BORDER')
-printI('then press `x`')
+print('Please move your mouse to the top left corner of the square item icon INCLUDING THE GREY BORDER')
+print('then press `x`\n')
 item_TL_loc = saveLocation(locationFile)
 
 
 #find item BR corner?? (or TR corner)
 print('WARNING: DO THIS NEXT INSTRUCTION CAREFULLY')
-print('Please move your mouse to the bottom right corner of any square item icon INCLUDING THE GREY BORDER')
-printI('then press `x`')
+print('Please move your mouse to the bottom right corner of the square item icon INCLUDING THE GREY BORDER')
+print('then press `x`\n')
 item_BR_loc = saveLocation(locationFile)
 
 #calculate BM ideal size based on these points
@@ -44,7 +46,7 @@ avg_dim = int((((personal_dim_1 + personal_dim_2)/2) // 5) * 5)
 locationFile.write(str(avg_dim) + '\n')
 
 #find purchase button
-print('Please move your mouse to the center of the Buy button corresponding to square item icon and press `x`')
+print('Please move your mouse to the center of the Buy button corresponding to square item icon and press `x`\n')
 buy_loc = saveLocation(locationFile)
 
 #calculate X and Y offset from the item's TL corner to the purchase button
@@ -54,7 +56,7 @@ locationFile.write(str(buy_offset_x) + ', ' + str(buy_offset_y) + '\n')
 
 
 #find purchase confirm button
-print('Press the Buy button and move your mouse over to the center of the confirm Buy button and press `x`')
+print('Press the Buy button and move your mouse over to the center of the confirm Buy button and press `x`\n')
 buyConfirm_loc = saveLocation(locationFile)
 
 
